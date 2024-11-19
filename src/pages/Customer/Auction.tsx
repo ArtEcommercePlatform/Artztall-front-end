ede84453922ad412427216dc2a680b889f7db3cb
19840769 <kodikarah.21@uom.lk>
2024-11-19 19:00:33 +0530

    updated Customer

---
+++ src/pages/Customer/Auction.tsx
@@ -0,0 +1,10 @@
+const Auctions = () => {
+    return (
+      <div className="p-6">
+        <h1 className="mb-6 text-2xl font-bold">Auctions</h1>
+        {/* Add your auctions content */}
+      </div>
+    );
+  };
+  
+  export default Auctions;
